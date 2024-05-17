dd9dbb80602217c01785f7b2a13630e37a77b1f4
Sam Jones <sam@refractar.com>
2024-05-17 14:57:15 -0600

    Initial Commit

---
+++ Ramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ramble
+//
+//  Created by Sam Jones on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
